14f52465268e10e2a0d92d058a511c2f6877e5a3
bidan <bidhan.adhikari@myy.haaga-helia.fi>
2025-05-11 21:40:24 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './index.css'
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="p-4 text-3xl font-bold text-blue-600 underline">
+        Hello world with Tailwind CSS!
+     </h1>
+    </div>
+  )
+}
+
+export default App
